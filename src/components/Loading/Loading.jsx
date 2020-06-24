be0284cea03bc26825156022ea565c11f6ee45ff
Alan Ayala <aaga941228@outlook.es>
2020-06-23 22:44:54 -0500

    loading centered

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 import './styles.css'
 
 const Loading = () => (
-  <div className="container-fluid m-5 p-5">
-    <div className="row m-5 p-5">
-      <div className="col m-5 p-5">
-        <div className="lds_ring"><div></div><div></div><div></div><div></div></div>
-      </div>
-    </div>
+  <div className="d-flex m-5 p-5 w-100 h-100 justify-content-center">
+    <div className="lds_ring"><div></div><div></div><div></div><div></div></div>
   </div>
 )
 
